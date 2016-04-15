7065bbfcc8e05a459338b7a6e396a501b930c1af
Coward123 <ywp8902@126.com>
2016-04-15 15:06:38 +0800

    Create timestamps--now2Hex.sh
    get unix time of current time;
    then shifting unix time to Hex time

---
+++ timestamps--now2Hex.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+now=`date +%s`
+tHex=''
+t=$now
+while (( $t > 0))
+do
+x=$(($t%16))
+case $x in
+[0-9])
+tHex=${x}${tHex}
+;;
+10)
+tHex='a'${tHex}
+;;
+11)
+tHex='b'${tHex}
+;;
+12)
+tHex='c'${tHex}
+;;
+13)
+tHex='d'${tHex}
+;;
+14)
+tHex='e'${tHex}
+;;
+15)
+tHex='f'${tHex}
+;;
+*)
+echo 'erro'
+break
+;;
+esac
+t=$(($t/16))
+done
+echo "now=$now"
+echo "tHex="$tHex
