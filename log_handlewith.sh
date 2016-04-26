1c8bcfdd387e8887a407b5dabc4cfef95c8bf4d5
Coward123 <ywp8902@126.com>
2016-04-26 22:10:25 +0800

    Create log_handlewith.sh

---
+++ log_handlewith.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#log_format
+#192.168.152.117 - - [26/Apr/2016:06:40:42 -0700] "GET http://jiandan.com/50x.html HTTP/1.1" 200 537 "-" "curl/7.19.7 (x86_64-redhat-linux-gnu) libcurl/7.19.7 NSS/3.14.0.0 zlib/1.2.3 libidn/1.18 libssh2/1.4.2" "-"
+#
+
+if [-z $1] ;then
+log=/usr/local/nginx/logs/access.log
+else
+log=$1
+fi
+
+if [ ! -f $log ];then
+echo 'file not exits......'
+exit(1)
+fi
+
+if[ -z $file ];then
+echo 'file is zero......'
+exit(1)
+fi
+
+awk '
+{
+if ( $9 ~ /200/){
+  ok_num++;
+  bit_num += $10
+}
+END{
+print "ok_num",ok_num;
+print "bit_num",bit_num;
+}
+' $log
+exit(0)
