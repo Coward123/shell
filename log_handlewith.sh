8b8daea5a599739efb543d5e91dfd9cdcc5e0e77
Coward123 <ywp8902@126.com>
2016-04-28 11:45:21 +0800

    Update log_handlewith.sh

--- log_handlewith.sh
+++ log_handlewith.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#shell：cat ./access.log|gawk '{print $4}'|sort|uniq -c|sort -nr
 #log_format
 #192.168.152.117 - - [26/Apr/2016:06:40:42 -0700] "GET http://jiandan.com/50x.html HTTP/1.1" 200 537 "-" "curl/7.19.7 (x86_64-redhat-linux-gnu) libcurl/7.19.7 NSS/3.14.0.0 zlib/1.2.3 libidn/1.18 libssh2/1.4.2" "-"
 #
